c98457f64829ab4fdda254c17145789f40419796
Alvaro A. Mora Acosta <alvaroandresmoraacosta95@gmail.com>
2026-04-22 10:43:37 +0100

    test 3

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import {
   UserGroupIcon,
   HomeIcon,
@@ -8,16 +7,24 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-
-
  
-// ...
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
  
 export default function NavLinks() {
-const pathname = usePathname();
+  const pathname = usePathname();
   return (
     <>
-      {links.smap((link) => {
+      {links.map((link) => {
         const LinkIcon = link.icon;
         return (
           <Link
@@ -28,7 +35,8 @@ const pathname = usePathname();
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
-            )}          >
+            )}
+>
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
@@ -37,3 +45,5 @@ const pathname = usePathname();
     </>
   );
 }
+ 
+ 
